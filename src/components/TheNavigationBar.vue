d9e583e7e1165267320704527f633ce5e0ff2e77
julio <juliod.rangelfarell@gmail.com>
2020-09-06 04:23:40 -0400

    Tab slider stays after route changes (workaround)

--- src/components/TheNavigationBar.vue
+++ src/components/TheNavigationBar.vue
@@ -1,120 +1,121 @@
 <template>
-  <v-container
-    fluid
-    fill-height
-  >
-    <v-row
-      justify="center"
-      align="center"
-      no-gutters
+  <div id="the-navigation-bar">
+    <v-container
+      fluid
+      fill-height
     >
-      <v-col
-        md="9"
-        class="relative"
+      <v-row
+        justify="center"
+        align="center"
+        no-gutters
       >
-        <v-app-bar
-          :color="backgroundColor"
-          app
-          flat
-          absolute
-          @mouseleave="resetSelectedTab"
+        <v-col
+          md="9"
+          class="relative"
         >
-          <v-img
-            id="navbar-logo"
-            :src="logoSource"
-            contain
-            min-height="3rem"
-            @click="handleClick"
-          />
+          <v-app-bar
+            :color="backgroundColor"
+            app
+            flat
+            absolute
+            @mouseleave="resetSelectedTab"
+          >
+            <v-img
+              id="navbar-logo"
+              :src="logoSource"
+              contain
+              min-height="3rem"
+              @click="handleClick"
+            />
 
-          <v-spacer />
+            <v-spacer />
 
-          <transition name="slide">
-            <v-tabs
-              v-show="showMenu"
-              :value="selectedTab"
-              class="hidden-sm-and-down"
-              right
-              optional
-              @change="resetSelectedTab"
-            >
-              <v-tab
-                to="/about"
-                @mouseover="setSelectedTab('/about')"
+            <transition name="slide">
+              <v-tabs
+                v-show="showMenu"
+                :value="selectedTab"
+                class="hidden-sm-and-down"
+                right
+                optional
               >
-                {{ $t('aboutUs') }}
-              </v-tab>
-              <v-tab
-                to="/projects"
-                @mouseover="setSelectedTab('/projects')"
-              >
-                {{ $t('projects') }}
-              </v-tab>
-              <v-tab
-                to="/contact"
-                @mouseover="setSelectedTab('/contact')"
-              >
-                {{ $t('contact') }}
-              </v-tab>
-              <v-tab
-                to="/blog"
-                @mouseover="setSelectedTab('/blog')"
-              >
-                {{ $t('blog') }}
-              </v-tab>
-              <v-menu
-                transition="scale-transition"
-              >
-                <template v-slot:activator="{ on }">
-                  <v-tab
-                    v-on="on"
-                    @mouseover="setSelectedTab(4)"
-                  >
-                    <v-icon small>
-                      {{ icons.mdiCogOutline }}
-                    </v-icon>
-                    {{ $t('settings') }}
-                  </v-tab>
-                </template>
+                <v-tab
+                  to="/about"
+                  @mouseover="setSelectedTab('/about')"
+                >
+                  {{ $t('aboutUs') }}
+                </v-tab>
+                <v-tab
+                  to="/projects"
+                  @mouseover="setSelectedTab('/projects')"
+                >
+                  {{ $t('projects') }}
+                </v-tab>
+                <v-tab
+                  to="/contact"
+                  @mouseover="setSelectedTab('/contact')"
+                >
+                  {{ $t('contact') }}
+                </v-tab>
+                <v-tab
+                  to="/blog"
+                  @mouseover="setSelectedTab('/blog')"
+                >
+                  {{ $t('blog') }}
+                </v-tab>
+                <v-menu
+                  transition="scale-transition"
+                >
+                  <template v-slot:activator="{ on }">
+                    <v-tab
+                      v-on="on"
+                      @mouseover="setSelectedTab(4)"
+                    >
+                      <v-icon small>
+                        {{ icons.mdiCogOutline }}
+                      </v-icon>
+                      {{ $t('settings') }}
+                    </v-tab>
+                  </template>
 
-                <v-list>
-                  <v-list-item @click.stop="toggleDarkMode">
-                    <v-list-item-action>
-                      <v-switch
-                        :input-value="darkModeEnabled"
-                        :value="darkModeEnabled"
-                      />
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>
-                        <v-icon small>
-                          {{ icons.mdiMoonWaningCrescent }}
-                        </v-icon>
-                        {{ $t('darkMode') }}
-                      </v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item @click.stop>
-                    <v-list-item-action>
-                      <v-icon> {{ icons.mdiEarth }}</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <LanguageSelector />
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </v-tabs>
-          </transition>
+                  <v-list>
+                    <v-list-item @click.stop="toggleDarkMode">
+                      <v-list-item-action>
+                        <v-switch
+                          :input-value="darkModeEnabled"
+                          :value="darkModeEnabled"
+                        />
+                      </v-list-item-action>
+                      <v-list-item-content>
+                        <v-list-item-title>
+                          <v-icon small>
+                            {{ icons.mdiMoonWaningCrescent }}
+                          </v-icon>
+                          {{ $t('darkMode') }}
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item @click.stop>
+                      <v-list-item-action>
+                        <v-icon> {{ icons.mdiEarth }}</v-icon>
+                      </v-list-item-action>
+                      <v-list-item-content>
+                        <LanguageSelector />
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </v-tabs>
+            </transition>
 
-          <v-app-bar-nav-icon
-            id="hamburger-icon"
-            @click.stop="toggleMenu"
-          />
-        </v-app-bar>
-      </v-col>
-    </v-row>
-  </v-container>
+            <v-app-bar-nav-icon
+              id="hamburger-icon"
+              @click.stop="toggleMenu"
+            />
+          </v-app-bar>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -156,6 +157,12 @@ export default {
     ]),
     logoSource() {
       return this.darkModeEnabled ? require('../assets/images/taller-db-logo-inverted.svg') : require('../assets/images/taller-db-logo.svg');
+    }
+  },
+  watch: {
+    $route() {
+      /* The v-tabs-slider fails to render when the route changes. Setting it to an invalid value forces an update of the internal component of v-tabs */
+      this.setSelectedTab(null)
     }
   },
   methods: {
