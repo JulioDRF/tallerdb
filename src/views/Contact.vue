0b23e41fa422504ebb363f3f218ef5a4441745ad
JulioDRF <juliod.rangelfarell@gmail.com>
2021-05-22 19:54:05 -0400

    Add FB and Instagram links with appropriate icons

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -11,7 +11,49 @@
       <v-row justify="center">
         <v-col class="min-height-50">
           <p>
-            <a href="mailto:contacto@tallerdb.com">contacto@tallerdb.com</a>
+            <a
+              href="mailto:contacto@tallerdb.com"
+              target="_blank"
+              class="mr-025"
+            >
+              <v-icon :dark="darkModeEnabled">
+                {{ mdiEmail }}
+              </v-icon>
+            </a>
+            <a
+              href="mailto:contacto@tallerdb.com"
+              target="_blank"
+            >contacto@tallerdb.com</a>
+          </p>
+          <p>
+            <a
+              href="https://www.facebook.com/taIIerdb"
+              target="_blank"
+              class="mr-025"
+            >
+              <v-icon :dark="darkModeEnabled">
+                {{ mdiFacebook }}
+              </v-icon>
+            </a>
+            <a
+              href="https://www.facebook.com/taIIerdb"
+              target="_blank"
+            >taIIerDB</a>
+          </p>
+          <p>
+            <a
+              href="https://www.instagram.com/taller_db_"
+              target="_blank"
+              class="mr-025"
+            >
+              <v-icon :dark="darkModeEnabled">
+                {{ mdiInstagram }}
+              </v-icon>
+            </a>
+            <a
+              href="https://www.instagram.com/taller_db_"
+              target="_blank"
+            >taller_DB_</a>
           </p>
         </v-col>
       </v-row>
@@ -20,8 +62,23 @@
 </template>
 
 <script>
+import { mdiEmail, mdiFacebook, mdiInstagram } from '@mdi/js'
+import { mapState } from 'vuex';
+
 export default {
   name: "Contact",
+  data() {
+    return {
+      mdiEmail,
+      mdiFacebook,
+      mdiInstagram,
+    }
+  },
+  computed: {
+    ...mapState([
+      'darkModeEnabled'
+    ]),
+  },
 };
 </script>
 
@@ -36,5 +93,8 @@ export default {
 }
 .min-height-50{
   min-height: 50vh;
+}
+.mr-025 {
+  margin-right: 0.25em;
 }
 </style>
